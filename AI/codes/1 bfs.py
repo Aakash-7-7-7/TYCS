9116186a5e836ee949132b9673630b2f52849e3e
Anuj Raj Vishwakarma <101452057+Anuj2026@users.noreply.github.com>
2024-10-04 11:49:55 +0530

    Add files via upload

---
+++ AI/codes/1 bfs.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+def bfs(graph, start):
+    visited =set()
+    queue = deque([start])
+
+    while queue:
+        vertex = queue.popleft()
+
+        if vertex not in visited:
+            visited.add(vertex)
+            print(vertex)
+
+            neighbors = graph[vertex]
+            for neighbor in neighbors:
+                if neighbor not in visited:
+                    queue.append(neighbor)
+
+graph ={
+    'A': [ 'B', 'C'],
+    'B': [ 'A', 'D', 'E'],
+    'C': [ 'A', 'F'],
+    'D': [ 'B'],
+    'E': [ 'A', 'F'],
+    'F': [ 'A', 'E']
+    }
+start_vertex ='A'
+bfs(graph, start_vertex)
+                
---
+++ AI/codes/1.1dfs pra.py
@@ -0,0 +1,20 @@
+def dfs(visited, graph, start):
+    if start not in visited:
+        visited.add(start)
+        print(start)
+        for neighbour in graph[start]:
+            dfs(visited, graph, neighbour)
+
+
+graph = {
+    '0':set(['1','2']),
+    '1':set(['1','3', '4']),
+    '2':set(['1','2']),
+    '3':set(['1','2']),
+    '4':set(['1','2','3']),
+
+    }
+visited= set()
+start = '0'
+dfs(visited ,graph, start)
+    
